4e8eb8234317c9423f33702b99a73ae448527af7
polizz <polizz@nowhere>
2026-01-10 19:09:31 -0600

    Add git_current_branch() fn to git aliases

--- .config/zsh/git/git.plugin.zsh
+++ .config/zsh/git/git.plugin.zsh
@@ -319,6 +319,17 @@ function ggfl() {
 }
 compdef _git ggfl=git-push
 
+function git_current_branch() {
+  local ref
+  ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
 alias gpsup='git push --set-upstream origin $(git_current_branch)'
 is-at-least 2.30 "$git_version" \
   && alias gpsupf='git push --set-upstream origin $(git_current_branch) --force-with-lease --force-if-includes' \
