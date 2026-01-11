18e7aa3c856d975fba61a723a3fd05885ad7c7a4
polizz <polizz@nowhere>
2026-01-10 18:44:07 -0600

    Add Mason ensure_installed for lsps

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -11,33 +11,36 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
+        "lua_ls", "clangd", "ts_ls", "pyright", "zls", "rust_analyzer", "marksman"
         -- add more arguments for adding more language servers
       })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        "stylua",
-        -- add more arguments for adding more null-ls sources
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
-        -- add more arguments for adding more debuggers
-      })
-    end,
-  },
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   -- overrides `require("mason-null-ls").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --       "prettier",
+  --       "stylua",
+  --       -- add more arguments for adding more null-ls sources
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   -- overrides `require("mason-nvim-dap").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --       "python",
+  --       -- add more arguments for adding more debuggers
+  --     })
+  --   end,
+  -- },
 }
+
+
+
