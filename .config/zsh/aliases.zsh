0ef2a02081451a96a082a136c2335676515b2077
polizz <polizz@nowhere>
2026-06-09 17:44:11 -0500

    Updates

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -54,5 +54,7 @@ alias crel='clang -std=c23 -Wall -Wextra -Wshadow -Werror -O2'
 newrepo() {
     ssh neptune "git init --bare /mnt/MAIN/git/$1.git && chown -R git:git /mnt/MAIN/git/$1.git" 
     git remote add origin git-neptune:$1.git
-    git push -u origin master
+    git push -u origin HEAD
 }
+
+alias claude_init='claude --print --model=haiku "Hello"'
